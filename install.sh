8d26dea574ec9648ad771b27c3b4f66835a393af
alvanrahimli <alvan@rahim.li>
2021-08-15 20:24:46 +0400

    Initial install script added

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "Installing dots-cli"
-wget
+echo "Downloading dots-cli"
+wget https://github.com/alvanrahimli/dots-cli/releases/download/v0.1/dots-cli-linux
+echo "Giving executable permission to app"
+chmod +x dots-cli-linux
+echo "Moving file to /usr/bin/"
+sudo mv dots-cli-linux /usr/bin/
+echo "Adding dots as alias to dots-cli-linux"
+echo 'alias dots="dots-cli-linux"' >> "$HOME"/.bashrc
+source "$HOME"/.bashrc
+
+echo "DONE! Your are ready to go!"
+echo "Run 'dots init myfirstpack' to initialize your first package"
