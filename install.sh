a767de3ee441412d8972df95520708d43ce7e2bc
alvanrahimli <alvan@rahim.li>
2021-08-15 20:28:53 +0400

    Refactoring

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Downloading dots-cli"
+if [ -f dots-cli-linux ]; then
+  echo "Existing dots-cli-linux file found. Deleting it."
+  rm -f dots-cli-linux
+fi
+
 wget https://github.com/alvanrahimli/dots-cli/releases/download/v0.1/dots-cli-linux
 echo "Giving executable permission to app"
 chmod +x dots-cli-linux
