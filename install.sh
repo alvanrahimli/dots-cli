dc9ad6f4156dd0a8207153b77bfc149801782968
alvanrahimli <alvan@rahim.li>
2021-08-21 21:24:48 +0400

    A bit better install script

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-echo "Downloading dots-cli"
+echo "  > Downloading dots-cli"
 if [ -f dots-cli-linux ]; then
-  echo "Existing dots-cli-linux file found. Deleting it."
+  echo "  > Existing dots-cli-linux file found. Deleting it."
   rm -f dots-cli-linux
 fi
 
-wget https://github.com/alvanrahimli/dots-cli/releases/download/v0.1/dots-cli-linux
-echo "Giving executable permission to app"
+wget -nv https://github.com/alvanrahimli/dots-cli/releases/download/v0.1/dots-cli-linux
+echo "  > Giving executable permission to app"
 chmod +x dots-cli-linux
-echo "Moving file to /usr/bin/"
-sudo mv dots-cli-linux /usr/bin/
-echo "Adding dots as alias to dots-cli-linux"
-echo 'alias dots="dots-cli-linux"' >> "$HOME"/.bashrc
-source "$HOME"/.bashrc
+echo "  > Moving file to /usr/bin/"
 
-echo "Creating config file"
+if [ -f /usr/bin/dots ]; then
+  echo "Executable with name dots found. Delete or move it to resolve conflict"
+  exit 0
+fi
+
+sudo mv dots-cli-linux /usr/bin/dots
+echo "  > Adding dots as alias to dots-cli-linux"
+echo 'alias dots="dots-cli-linux"' >> "$HOME"/.bashrc
+
+echo "  > Creating config file"
 mkdir "$HOME"/.config/dots-cli/
 curl https://raw.githubusercontent.com/alvanrahimli/dots-cli/master/config.json > "$HOME"/.config/dots-cli/config.json
 
-echo "DONE! Your are ready to go!"
-echo "Run 'dots init myfirstpack' to initialize your first package"
+echo "  > DONE! Your are ready to go!"
+echo "  > Restart terminal"
+echo "  > Run 'dots init myfirstpack' to initialize your first package"
